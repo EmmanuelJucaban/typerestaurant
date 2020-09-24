f881b0f6c0d9ac77b204261a69fa49d3e2af65d9
emmanueljucaban <emmanueljucaban@gmail.com>
2020-09-23 19:23:19 -0700

    F - Created front end html

--- index.ts
+++ index.ts
@@ -1,19 +1,13 @@
 import express from 'express';
 import routes from './routes';
-
 const app = express();
 // const PORT: number = 3000;
 const PORT = 3000;
-
 app.use(express.static('public'));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // '/'
 app.use(routes);
-
-
 app.listen(3000, () => {
   console.log('Server started listening on http://localhost:3000');
 });
