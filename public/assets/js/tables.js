cb7b0852a84d0c3eb20b6eb17864c0a3798ecce3
emmanueljucaban <emmanueljucaban@gmail.com>
2020-09-23 19:58:34 -0700

    F - Created api request and rendered the reservations table

---
+++ public/assets/js/tables.js
@@ -0,0 +1,27 @@
+$(document).ready(async () => {
+  const $reservations = $('#reservation');
+  const reservations = await $.get('/api/reservation');
+  const waitlists = await $.get('/api/waitlist');
+  console.log('RESERVATIONS!!!!', reservations);
+  console.log('Waitlists!!!!', waitlists);
+  reservations.forEach((reservation, index) => {
+    const $li = $('<li>').addClass("list-group-item mt-4");
+    const $h2 = $('<h2>').text(`Table #${index + 1}`);
+    const $hr = $('<hr>');
+    const $h2Id = $('<h2>').text(`ID: ${reservation.id}`);
+    const $h2Name = $('<h2>').text(`Name: ${reservation.name}`);
+    const $h2Email = $('<h2>').text(`Email: ${reservation.email}`);
+    const $h2Phone = $('<h2>').text(`Phone: ${reservation.phone}`);
+    $li.append($h2, $hr, $h2Id, $h2Name, $h2Email, $h2Phone);
+    $reservations.append($li);
+  });
+  // <li class="list-group-item mt-4">
+  //   <h2>Table #1</h2>
+  //   <hr>
+  //     <h2>ID: afhaque89</h2>
+  //     <h2>Name: Ahmed</h2>
+  //     <h2>Email: ahmed@example.com</h2>
+  //     <h2>Phone: 000-000-0000</h2>
+  // </li>
+});
+
